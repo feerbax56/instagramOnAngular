84ce455da2b02eac1ea05ca39e81dba7763401b5
Evseev Anton <soki1997@mail.ru>
2023-02-21 07:10:59 +0500

    add: 09.2 NgFor сложная структура данных

--- src/app/parent/parent/parent.component.ts
+++ src/app/parent/parent/parent.component.ts
@@ -1,13 +1,63 @@
 import { Component } from '@angular/core'
 
+interface weekGrades {
+  id: number
+  gradeItem: number
+}
+
+interface Lesson {
+  id: number
+  title: string
+  weekGrades: weekGrades[]
+}
+
 @Component({
   selector: 'inst-parent',
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.css'],
 })
 export class ParentComponent {
-  grades: string[] = ['math: 5', 'english: 3']
+  // grades: string[] = ['math: 5', 'english: 3']
+  lessons: Lesson[] = [
+    {
+      id: 0,
+      title: 'Math',
+      weekGrades: [
+        {
+          id: 0,
+          gradeItem: 5,
+        },
+        {
+          id: 1,
+          gradeItem: 2,
+        },
+        {
+          id: 2,
+          gradeItem: 3,
+        },
+      ],
+    },
+    {
+      id: 1,
+      title: 'Physic',
+      weekGrades: [
+        {
+          id: 0,
+          gradeItem: 3,
+        },
+        {
+          id: 1,
+          gradeItem: 4,
+        },
+        {
+          id: 2,
+          gradeItem: 5,
+        },
+      ],
+    },
+  ]
+
   getGrade(grade: string) {
-    this.grades.push(grade)
+    // this.grades.push(grade)
   }
 }
